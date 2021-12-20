32876a943a180669a3db0cb58c592e02b9dac0a8
Max Roberto <max.roberto@leggett.com>
2021-12-20 09:17:18 -0300

    PrimeiraAulaJS

---
+++ PrimeiraAula.js
@@ -0,0 +1,17 @@
+const num1 = 2;
+const num2 = 3;
+const resultInt = num1 + num2;
+
+console.log(resultInt)
+
+const num3 = 1.2;
+const num4 = .3;
+const resultDec = num3 + num4;
+
+console.log(resultDec)
+
+const PrimeiraString = "Max diz: "
+const SegundaString = "'OK'"
+const resultString = PrimeiraString + SegundaString
+
+console.log(resultString)
